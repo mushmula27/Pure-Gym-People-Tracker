5900499ab4db032038324a80effb595ae05f1939
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2018-09-02 20:42:23 +0100

    Added functions to filter and take average of people

--- Graphing/DataVisualisation.js
+++ Graphing/DataVisualisation.js
@@ -1,9 +1,46 @@
-console.log("Yamum.")
+console.log("Yamum.");
 
-axios.get("https://playground.hungryturtlecode.com/api/puregym/data")
+// Scrape data from the API
+axios
+  .get("https://playground.hungryturtlecode.com/api/puregym/data")
   .then(res => {
     data = res.data;
-    console.log(data)
 
-    //moment.js lookup to make timestamp in usable format
+    // Apply moment.js to the timestamps in data.
+    // Create new object with keys Number of people and Timestamp where
+    // Number of people is converted to int and
+    // Timestamp has moment.js applied to it.
+    var dataMoment = data.map(x => {
+      return {
+        "Number of people": parseInt(x["Number of people"]),
+        Timestamp: moment(x.Timestamp)
+      };
+    });
+    // Group dataMoment by date(default)/time/whatever
+    var grouped = groupBy(dataMoment, "Timestamp");
+
+    var filter = "1 Aug 2018";
+    var total = totalPeeps(grouped, filter);
+    var ave = Math.round(total / grouped[filter].length);
   });
+
+// Group data in any format. Default is "D MMM YYYY".
+//Declare any other format as argument in groupBy function
+function groupBy(objectArray, property, dateFormat = "D MMM YYYY") {
+  return objectArray.reduce((acc, obj) => {
+    var key = obj[property];
+    var keyF = key.format(dateFormat);
+    if (!acc[keyF]) {
+      acc[keyF] = [];
+    }
+    acc[keyF].push(obj);
+    return acc;
+  }, {});
+}
+
+function totalPeeps(objectArray, date) {
+  return objectArray[date].reduce(
+    (acc, currentVal) => acc + currentVal["Number of people"],
+    0
+  );
+}
