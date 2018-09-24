53d2a2bdf2a0211413454566073f4cb1eab7284c
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2018-09-24 20:36:39 +0100

    Introducing class

--- Graphing/DataVisualisation.js
+++ Graphing/DataVisualisation.js
@@ -1,87 +1,67 @@
 console.log("Yamum.");
 
+class Parameters {
+  constructor(dataMoment, plot, groupingParam1, groupingParam2) {
+    this.dataMoment = dataMoment;
+    this.plot = plot;
+    this.groupingParam1 = groupingParam1;
+    this.groupingParam2 = groupingParam2;
+  }
+}
+
+const dataObject = new Parameters(dataMoment, plot, hour, dayOfWeek);
+loadMaData();
 // Scrape data from the API
-axios
-  .get("https://playground.hungryturtlecode.com/api/puregym/data")
-  .then(res => {
-    var data = res.data;
+function loadMaData() {
+  axios
+    .get("https://playground.hungryturtlecode.com/api/puregym/data")
+    .then(res => {
+      var data = res.data;
 
-    // Apply moment.js to the timestamps in data.
-    // Create new object with keys Number of people and Timestamp where
-    // Number of people is converted to int and
-    // Timestamp has moment.js applied to it.
-    var dataMoment = data.map(x => {
-      return {
-        "Number of people": parseInt(x["Number of people"]),
-        Timestamp: moment(x.Timestamp)
+      // Apply moment.js to the timestamps in data.
+      // Create new object with keys Number of people and Timestamp where
+      // Number of people is converted to int and
+      // Timestamp has moment.js applied to it.
+      var dataMoment = data.map(x => {
+        return {
+          "Number of people": parseInt(x["Number of people"]),
+          Timestamp: moment(x.Timestamp)
+        };
+      });
+      // Getting current number of people in the gym
+      getCurNum(dataMoment);
+      // Group dataMoment by date(default)/time/whatever
+      var day = "D MMM YYYY";
+      var month = "MMM YYYY";
+      var year = "YYYY";
+      var dayOfWeek = "ddd";
+      var hour = "HH";
+      //var hpd = "HH ddd";
+
+      // Prep empty plot variable
+      var plot = {
+        labels: [],
+        series: []
       };
+      var seriesData = [];
+
+      // // Single bar plots
+      // var grouped = groupBy(dataMoment, "Timestamp", hour);
+      // var keys = Object.keys(grouped);
+      //
+      // for (let i = 0; i < keys.length; i++) {
+      //   filter = keys[i];
+      //   total = totalPeeps(grouped, filter);
+      //   ave = average(total, grouped, filter);
+      //
+      //   plot["labels"].push(filter);
+      //   seriesData.push(ave);
+      // }
+      // plot["series"].push(seriesData);
+
+      plotChart(plot);
     });
-    // Getting current number of people in the gym
-    getCurNum(dataMoment);
-    // Group dataMoment by date(default)/time/whatever
-    var day = "D MMM YYYY";
-    var month = "MMM YYYY";
-    var year = "YYYY";
-    var dayOfWeek = "ddd";
-    var hour = "HH";
-    //var hpd = "HH ddd";
-
-    // Prep empty plot variable
-    var plot = {
-      labels: [],
-      series: []
-    };
-    var seriesData = [];
-
-    // // Single bar plots
-    var grouped = groupBy(dataMoment, "Timestamp", hour);
-    var keys = Object.keys(grouped);
-    //
-    // for (let i = 0; i < keys.length; i++) {
-    //   filter = keys[i];
-    //   total = totalPeeps(grouped, filter);
-    //   ave = average(total, grouped, filter);
-    //
-    //   plot["labels"].push(filter);
-    //   seriesData.push(ave);
-    // }
-    // plot["series"].push(seriesData);
-
-    // Stacked plots
-    var sorted = {};
-
-    for (let i = 0; i < keys.length; i++) {
-      sorted[keys[i]] = groupBy(grouped[keys[i]], "Timestamp", dayOfWeek);
-    }
-    var keysSorted = Object.keys(sorted);
-    // plot["labels"] = keysSorted;
-    var subkey = [];
-    for (let i = 0; i < keysSorted.length; i++) {
-      subkey = Object.keys(sorted[keysSorted[i]]);
-      var total = [];
-      var ave = [];
-      for (let p = 0; p < subkey.length; p++) {
-        total[p] = totalPeeps(sorted[keysSorted[i]], subkey[p]);
-        ave[p] = average(total[p], sorted[keysSorted[i]], subkey[p]);
-      }
-      plot["series"].push(ave);
-    }
-    plot["labels"] = subkey;
-
-    // Plotting the data
-
-    var chart = new Chartist.Bar("#chart1", plot);
-    chart.on("draw", function(context) {
-      if (context.type === "bar") {
-        context.element.attr({
-          style:
-            "stroke: hsl(" +
-            Math.floor((Chartist.getMultiValue(context.value) / 200) * 255) +
-            ", 50%, 50%);"
-        });
-      }
-    });
-  });
+}
 
 ////// FUNCTIONS /////
 function getCurNum(data) {
@@ -117,8 +97,50 @@ function average(total, data, filter) {
   return Math.round(total / data[filter].length);
 }
 
+// Stacked plots
+function stackedPlot(data, groupingParam1, groupingParam2) {
+  var grouped = groupBy(data, "Timestamp", groupingParam1);
+  var keys = Object.keys(grouped);
+  var sorted = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    sorted[keys[i]] = groupBy(grouped[keys[i]], "Timestamp", groupingParam2);
+  }
+  var keysSorted = Object.keys(sorted);
+  // plot["labels"] = keysSorted;
+  var subkey = [];
+  for (let i = 0; i < keysSorted.length; i++) {
+    subkey = Object.keys(sorted[keysSorted[i]]);
+    var total = [];
+    var ave = [];
+    for (let p = 0; p < subkey.length; p++) {
+      total[p] = totalPeeps(sorted[keysSorted[i]], subkey[p]);
+      ave[p] = average(total[p], sorted[keysSorted[i]], subkey[p]);
+    }
+    plot["series"].push(ave);
+  }
+  plot["labels"] = subkey;
+}
+
+// Plotting the data
+function plotChart(plot) {
+  chart = new Chartist.Bar("#chart1", plot);
+  chart.on("draw", function(context) {
+    if (context.type === "bar") {
+      context.element.attr({
+        style:
+          "stroke: hsl(" +
+          Math.floor((Chartist.getMultiValue(context.value) / 200) * 255) +
+          ", 50%, 50%);"
+      });
+    }
+  });
+}
+
 var button = document.getElementById("get");
 
 button.addEventListener("click", () => {
   console.log("button was licked.");
+  stackedPlot(dataMoment, hour, dayOfWeek);
+  plotChart(plot);
 });
